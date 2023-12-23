46f3a7d6a132b0065602b6decaac3ca9d2b9046b
Max Lee <maxjerry0107@gmail.com>
2023-12-23 03:38:11 -0500

    product title bottom margin increased

--- src/components/product/product-description.svelte
+++ src/components/product/product-description.svelte
@@ -10,7 +10,7 @@
 
 {#if product}
 	<div class="mb-6 flex flex-col border-b pb-6 dark:border-neutral-700">
-		<h1 class="mb-2 text-5xl font-medium">{product.title}</h1>
+		<h1 class="mb-5 text-5xl font-medium">{product.title}</h1>
 		<div class="mr-auto w-auto rounded-full bg-blue-600 p-2 text-sm text-white">
 			<Price
 				amount={product.priceRange.maxVariantPrice.amount}
